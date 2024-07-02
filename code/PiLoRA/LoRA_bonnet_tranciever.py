a8daa5507923adfeb0a59bfaaeda2c642309f3fe
Abrar Hossain <abrarhossainhimself@gmail.com>
2024-07-02 15:37:18 -0600

    Update LoRA_bonnet_tranciever.py

--- code/PiLoRA/LoRA_bonnet_tranciever.py
+++ code/PiLoRA/LoRA_bonnet_tranciever.py
@@ -1,75 +1,61 @@
-
 import board
 import busio
 import digitalio
-
 import adafruit_rfm9x
-
+import paho.mqtt.client as mqtt
+import json
 
 # Define radio parameters.
-RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
-# module! Can be a value like 915.0, 433.0, etc.
+RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your module!
+# Can be a value like 915.0, 433.0, etc.
 
-# Define pins connected to the chip, use these if wiring up the breakout according to the guide:
+# Define pins connected to the chip
 CS = digitalio.DigitalInOut(board.CE1)
 RESET = digitalio.DigitalInOut(board.D25)
-# Or uncomment and instead use these if using a Feather M0 RFM9x board and the appropriate
-# CircuitPython build:
-# CS = digitalio.DigitalInOut(board.RFM9X_CS)
-# RESET = digitalio.DigitalInOut(board.RFM9X_RST)
-
-# Define the onboard LED
-#LED = digitalio.DigitalInOut(board.D13)
-#LED.direction = digitalio.Direction.OUTPUT
 
 # Initialize SPI bus.
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-# Initialze RFM radio
+# Initialize RFM radio
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+rfm9x.tx_power = 23  # Set transmit power (in dB)
 
-# Note that the radio is configured in LoRa mode so you can't control sync
-# word, encryption, frequency deviation, or other settings!
+# Define MQTT parameters
+MQTT_BROKER = "iotwx.ucar.edu"  # Local MQTT broker
+MQTT_PORT = 1883
+MQTT_TOPIC = "test/topic"  # Example topic
 
-# You can however adjust the transmit power (in dB).  The default is 13 dB but
-# high power radios like the RFM95 can go up to 23 dB:
-rfm9x.tx_power = 23
+# MQTT callback functions
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
 
-# Send a packet.  Note you can only send a packet up to 252 bytes in length.
-# This is a limitation of the radio packet size, so if you need to send larger
-# amounts of data you will need to break it into smaller send calls.  Each send
-# call will wait for the previous one to finish before continuing.
-#rfm9x.send(bytes("Hello world!\r\n", "utf-8"))
-print("Sent Hello World message!")
+# Initialize MQTT client
+mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_connect
+mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
+mqtt_client.loop_start()
 
-# Wait to receive packets.  Note that this library can't receive data at a fast
-# rate, in fact it can only receive and process one 252 byte packet at a time.
-# This means you should only use this for low bandwidth scenarios, like sending
-# and receiving a single message at a time.
 print("Waiting for packets...")
 
 while True:
-    rfm9x.send(bytes("Hello rp1!\r\n", "utf-8"))
     packet = rfm9x.receive(timeout=5.0)
-    # Optionally change the receive timeout from its default of 0.5 seconds:
-    # packet = rfm9x.receive(timeout=5.0)
-    # If no packet was received during the timeout then None is returned.
     if packet is None:
-        # Packet has not been received
-        #LED.value = False
         print("Received nothing! Listening again...")
     else:
-        # Received a packet!
-        #LED.value = True
-        # Print out the raw bytes of the packet:
         print("Received (raw bytes): {0}".format(packet))
-        # And decode to ASCII text and print it too.  Note that you always
-        # receive raw bytes and need to convert to a text format like ASCII
-        # if you intend to do string processing on your data.  Make sure the
-        # sending side is sending ASCII data before you try to decode!
         packet_text = str(packet, "ascii")
         print("Received (ASCII): {0}".format(packet_text))
-        # Also read the RSSI (signal strength) of the last received message and
-        # print it.
         rssi = rfm9x.last_rssi
         print("Received signal strength: {0} dB".format(rssi))
+
+        # Prepare data for MQTT
+        data = {
+            "packet": packet_text,
+            "rssi": rssi
+        }
+
+        # Publish data to MQTT
+        mqtt_client.publish(MQTT_TOPIC, json.dumps(data))
+
+mqtt_client.loop_stop()
+mqtt_client.disconnect()
